a5461e06be720d248db46e4ff806e776ef5d0ccf
Hamza <hamza.aboujaib.0303@gmail.com>
2023-02-07 15:47:11 -0500

    Create logic to add colors to segments

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,8 +1,10 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Random;
 
@@ -35,6 +37,7 @@ public class DotGen {
         }
         // Distribute colors randomly. Vertices are immutable, need to enrich them
         ArrayList<Vertex> verticesWithColors = new ArrayList<>();
+        ArrayList<Segment> segmentsWithColors = new ArrayList<>();
         Random bag = new Random();
         for(Vertex v: vertices){
             int red = bag.nextInt(255);
@@ -46,7 +49,38 @@ public class DotGen {
             verticesWithColors.add(colored);
         }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments).build();
+        for(Segment s: segments){
+            Vertex v1 = verticesWithColors.get(s.getV1Idx());
+            Vertex v2 = verticesWithColors.get(s.getV2Idx());
+            Color v1Color = extractColor(v1.getPropertiesList());
+            Color v2Color = extractColor(v2.getPropertiesList());
+            int segmentRed = (v1Color.getRed() + v2Color.getRed())/2;
+            int segmentGreen = (v1Color.getGreen() + v2Color.getGreen())/2;
+            int segmentBlue = (v1Color.getBlue() + v2Color.getBlue())/2;
+            String colorCode = segmentRed + "," + segmentGreen + "," + segmentBlue;
+            Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
+            Segment colored = Segment.newBuilder(s).addProperties(color).build();
+            segmentsWithColors.add(colored);
+        }
+
+        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segmentsWithColors).build();
     }
 
+
+    private Color extractColor(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("rgb_color")) {
+                //System.out.println(p.getValue());
+                val = p.getValue();
+            }
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        return new Color(red, green, blue);
+    }
 }
