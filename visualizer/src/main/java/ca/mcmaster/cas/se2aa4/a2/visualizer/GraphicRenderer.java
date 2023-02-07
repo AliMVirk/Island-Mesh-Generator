9f270476558750806f41910736fd1a8364234916
Ali Virk <virka9@mcmaster.ca>
2023-02-07 14:40:56 -0500

    Draw all segments

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -3,6 +3,7 @@ package ca.mcmaster.cas.se2aa4.a2.visualizer;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 import java.awt.Graphics2D;
 import java.awt.Stroke;
@@ -10,6 +11,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.util.List;
+import java.awt.geom.Line2D;
 
 public class GraphicRenderer {
 
@@ -27,13 +29,23 @@ public class GraphicRenderer {
             canvas.fill(point);
             canvas.setColor(old);
         }
+        canvas.setStroke(new BasicStroke(0.25f));
+        for (Segment s : aMesh.getSegmentsList()) {
+            Vertex v1 = aMesh.getVertices(s.getV1Idx());
+            Vertex v2 = aMesh.getVertices(s.getV2Idx());
+            Color old = canvas.getColor();
+            canvas.setColor(Color.BLACK); // change color to average of v1 and v2
+            Line2D line = new Line2D.Double(v1.getX(), v1.getY(), v2.getX(), v2.getY());
+            canvas.draw(line);
+            canvas.setColor(old);
+        }
     }
 
     private Color extractColor(List<Property> properties) {
         String val = null;
         for(Property p: properties) {
             if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
+                //System.out.println(p.getValue());
                 val = p.getValue();
             }
         }
