f7e1b5f96e5294015e9a6e782cd234034226824a
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-21 19:53:37 -0500

    Create a class to handle the generation of segments

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/SegmentGen.java
@@ -0,0 +1,81 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+
+public class SegmentGen {
+
+    public Mesh generate(Mesh mesh){
+        //Create all the segments builders
+        ArrayList<Segment.Builder> segmentBuilders = initializeSegments();
+        // Attribute thickness and color to segments
+        segmentBuilders = addSegmentProperties(segmentBuilders, mesh.getVerticesList(), null, 0.25f);
+
+        // Build all the segments
+        ArrayList<Segment> segments = new ArrayList<>();
+        for (Segment.Builder s : segmentBuilders)
+            segments.add(s.build());
+
+        return Mesh.newBuilder().addAllVertices(mesh.getVerticesList()).addAllSegments(segments).build();
+    }
+
+    private ArrayList<Segment.Builder> initializeSegments() {
+        ArrayList<Segment.Builder> segmentBuilders = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 25; j++) {
+                if (j != 24)
+                    segmentBuilders.add(Segment.newBuilder().setV1Idx(j+i*25).setV2Idx(j+i*25+1));
+                if (i != 24)
+                    segmentBuilders.add(Segment.newBuilder().setV1Idx(j+i*25).setV2Idx(j+i*25+25));
+            }
+        }
+        return segmentBuilders;
+    }
+
+    private ArrayList<Segment.Builder> addSegmentProperties(ArrayList<Segment.Builder> segmentBuilders, List<Vertex> vertexBuilders, String rgbValue, float thickness) {
+        String colorCode = rgbValue;
+        for(Segment.Builder s : segmentBuilders){
+            // Color segments by averaging out adjacent vertex colors if there is no given color
+            if (rgbValue == null) {
+                Vertex v1 = vertexBuilders.get(s.getV1Idx());
+                Vertex v2 = vertexBuilders.get(s.getV1Idx());
+                Color v1Color = extractColor(v1.getPropertiesList());
+                Color v2Color = extractColor(v2.getPropertiesList());
+                int segmentRed = (v1Color.getRed() + v2Color.getRed())/2;
+                int segmentGreen = (v1Color.getGreen() + v2Color.getGreen())/2;
+                int segmentBlue = (v1Color.getBlue() + v2Color.getBlue())/2;
+                colorCode = segmentRed + "," + segmentGreen + "," + segmentBlue;
+            }
+            // Set segment color
+            Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
+            s.addProperties(color);
+            // Assign segment thickness
+            s.addProperties(Property.newBuilder().setKey("thickness").setValue(String.valueOf(thickness)).build());
+        }
+        return segmentBuilders;
+    }
+
+    private Color extractColor(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("rgb_color"))
+                val = p.getValue();
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        int transparency = 255;
+        if (raw.length == 4)
+            transparency = Integer.parseInt(raw[3]);
+        return new Color(red, green, blue, transparency);
+    }
+}
