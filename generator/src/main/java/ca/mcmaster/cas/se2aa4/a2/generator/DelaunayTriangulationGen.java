476380694cedb7656382a61e6b2512f8fd0c1d82
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 10:01:03 -0500

    Create a coords list of polygon centroids and use Delaunay Triangulation to create the triangles

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DelaunayTriangulationGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DelaunayTriangulationGen.java
@@ -1,11 +1,30 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.triangulate.DelaunayTriangulationBuilder;
+
+import java.util.ArrayList;
 
 public class DelaunayTriangulationGen {
 
     public Mesh.Builder generate(Mesh.Builder mesh){
+        GeometryFactory geometryFactory = new GeometryFactory();
+        ArrayList<Coordinate> coords = new ArrayList<>();
+        // Set sites to be already existing vertices in the mesh
+        for (Structs.Polygon p : mesh.getPolygonsList()) {
+            Structs.Vertex centroid = mesh.getVertices(p.getCentroidIdx());
+            coords.add(new Coordinate(centroid.getX(), centroid.getY()));
+        }
 
+        DelaunayTriangulationBuilder triangleBuilder = new DelaunayTriangulationBuilder();
+        triangleBuilder.setSites(coords);
+
+        // Creates the triangles
+        Geometry triangles = triangleBuilder.getTriangles(geometryFactory);
 
 
         return mesh;
