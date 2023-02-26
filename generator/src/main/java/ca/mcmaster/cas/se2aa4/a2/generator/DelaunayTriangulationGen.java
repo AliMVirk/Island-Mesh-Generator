c5e9c1f594e8d569165296c05d743904da9e434c
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 09:47:53 -0500

    Create file for DelaunayTriangulationGen and create the skeleton of of the generate function

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DelaunayTriangulationGen.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+
+public class DelaunayTriangulationGen {
+
+    public Mesh.Builder generate(Mesh.Builder mesh){
+
+
+
+        return mesh;
+    }
+
+
+}
