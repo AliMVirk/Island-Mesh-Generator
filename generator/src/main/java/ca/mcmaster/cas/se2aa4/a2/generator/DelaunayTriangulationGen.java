23677180884fc9348142c3ea9db0f6e1cb86dfd5
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-02-26 10:06:01 -0500

    Convert triangles from JTS Geometry to JTS Polygon

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DelaunayTriangulationGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DelaunayTriangulationGen.java
@@ -2,9 +2,7 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.*;
 import org.locationtech.jts.triangulate.DelaunayTriangulationBuilder;
 
 import java.util.ArrayList;
@@ -25,6 +23,16 @@ public class DelaunayTriangulationGen {
 
         // Creates the triangles
         Geometry triangles = triangleBuilder.getTriangles(geometryFactory);
+        // Polygon here is JTS Polygon
+        ArrayList<Polygon> trianglesProduced = new ArrayList<>();
+
+        if(triangles instanceof GeometryCollection geometryCollection) {
+            for(int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+                // Convert triangles from Geometry into Polygons and add them to triangles produced list
+                trianglesProduced.add((Polygon) geometryCollection.getGeometryN(i));
+            }
+        }
+
 
 
         return mesh;
