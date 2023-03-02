e4527a7a37648947eb54493788161dc99ea7b604
Ali Virk <ali.m.virk@outlook.com>
2023-03-02 09:50:34 -0500

    Create method to fill polygons based on tile type

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -11,8 +11,10 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 import java.util.List;
 import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 
 
 public class GraphicRenderer {
@@ -41,6 +43,8 @@ public class GraphicRenderer {
             // Draw neighboring relations in grey when debug mode is active
             drawNeighborRelations(aMesh, canvas);
         } else {
+            // Draw tiles
+            drawTiles(aMesh, canvas, irregular);
             // Draw vertices
             drawVertices(aMesh, canvas, 0, divider, null);
             // Draw segments
@@ -103,6 +107,40 @@ public class GraphicRenderer {
                 Line2D line = new Line2D.Double(getX(v1), getY(v1), getX(v2), getY(v2));
                 canvas.draw(line);
             }
+        }
+    }
+
+    private void drawTiles(Mesh aMesh, Graphics2D canvas, boolean isIrregular) {
+        Color color = Color.BLACK;
+        for (Polygon p : aMesh.getPolygonsList()) {
+            // Set color based on polygon tile type
+            for (Property q : p.getPropertiesList()) {
+                if (q.getKey().equals("tile_type")) {
+                    if (q.getValue().equals("land"))
+                        color = Color.ORANGE;
+                    else if (q.getValue().equals("water"))
+                        color = Color.BLUE;
+                }
+            }
+            canvas.setColor(color);
+            canvas.setStroke(new BasicStroke(extractThickness(p.getPropertiesList())));
+            // Create polygon shape to fill
+            Path2D path = new Path2D.Double();
+            // Initialize path with v0
+            Vertex v0 = aMesh.getVertices(aMesh.getSegments(p.getSegmentIdxs(0)).getV1Idx());
+            path.moveTo(getX(v0), getY(v0));
+            // Create shape to draw based on mesh regularity type
+            for (int i = (isIrregular) ? 0 : 1; i < p.getSegmentIdxsCount(); i++) {
+                Segment s = aMesh.getSegments(p.getSegmentIdxsList().get(i));
+                Vertex v;
+                if (i != p.getSegmentIdxsCount() - 1 || isIrregular)
+                    v = aMesh.getVertices(s.getV2Idx());
+                else
+                    v = aMesh.getVertices(s.getV1Idx());
+                path.lineTo(getX(v), getY(v));
+            }
+            path.closePath();
+            canvas.fill(path);
         }
     }
 
