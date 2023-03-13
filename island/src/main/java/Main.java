83cb375a34627706e87a2bf5d11456f63d2ba513
Ali Virk <ali.m.virk@outlook.com>
2023-03-13 19:25:42 -0400

    Remove unused import statements

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,15 +1,5 @@
 import java.io.IOException;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-
-import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
-import island.*;
 import island.configuration.MeshConfiguration;
 
 public class Main {
