5af8de812bea3be910c6ee92563760d4737e2985
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-15 21:43:09 -0400

    Expand Tile class to include more configuration and altitude for F19

--- island/src/main/java/island/Tile/Tile.java
+++ island/src/main/java/island/Tile/Tile.java
@@ -3,20 +3,38 @@ package island.Tile;
 import java.awt.*;
 
 public class Tile {
-    private final Type type;
-    private final Color color;
+    private Type type;
+    private Color color;
+    private double altitude;
 
     public Tile (Type type, Color color){
         this.type = type;
         this.color = color;
+        this.altitude = 0;
     }
 
     public String getType(){
         return this.type.toString();
     }
 
+    public void setType(Type type) {
+        this.type = type;
+    }
+
     public String getColor(){
         return Integer.toString(this.color.getRGB());
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getAltitude(){
+        return this.altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
     }
 
 }
