33298a327805e1f0298ebd4702649731b073b954
Ali Virk <ali.m.virk@outlook.com>
2023-03-23 18:51:41 -0400

    Create enrichment generator to add humidity, moisture, and vegetation to land tiles for F22

---
+++ island/src/main/java/island/generators/EnrichmentGen.java
@@ -0,0 +1,17 @@
+package island.generators;
+
+import java.util.List;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import island.Tile.River;
+import island.Tile.Tile;
+
+public class EnrichmentGen {
+    
+    public List<Tile> enrichLand(Mesh oMesh, List<Tile> tiles, River[] rivers) {
+
+
+        return tiles;
+    }
+
+}
