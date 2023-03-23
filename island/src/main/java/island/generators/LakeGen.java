5aaff3d70ebea0969ba9b945a99942e18b8a9ee5
Ali Virk <ali.m.virk@outlook.com>
2023-03-23 18:07:26 -0400

    Remove LakeTile class dependencies in LakeGen class

--- island/src/main/java/island/generators/LakeGen.java
+++ island/src/main/java/island/generators/LakeGen.java
@@ -3,8 +3,8 @@ package island.generators;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import island.Tile.Tile;
 import island.Tile.Type;
-import island.Tiles.LakeTile;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -20,12 +20,16 @@ public class LakeGen {
         for (int j = 0; j < numOfLakes && polygonIdxs.size() != 0; j++) {
             // Set random valid tile as a lake
             int index = polygonIdxs.get(rnd.nextInt(polygonIdxs.size()));
-            tiles.set(index, new LakeTile(100));
+            Tile lakeTile = new Tile(Type.WATER, new Color(1, 64, 98));
+            lakeTile.setAltitude(100);
+            tiles.set(index, lakeTile);
             polygonIdxs.remove((Object) index);
             // Go through the lake's neighbors and randomly set some as lakes
             for (int n : oMesh.getPolygons(index).getNeighborIdxsList()){
                 if (rnd.nextBoolean() && polygonIdxs.contains(n)) {
-                    tiles.set(n, new LakeTile(100));
+                    lakeTile = new Tile(Type.WATER, new Color(1, 64, 98));
+                    lakeTile.setAltitude(100);
+                    tiles.set(n, lakeTile);
                     polygonIdxs.remove((Object) n);
                 }
             }
@@ -39,10 +43,10 @@ public class LakeGen {
         List<Integer> validPolygonIdxs = new ArrayList<>();
         for (int i = 0; i < mesh.getPolygonsCount(); i++) {
             boolean valid = true;
-            if (tiles.get(i).getType().equals(Type.WATER.toString()) || tiles.get(i).getType().equals(Type.LAKE.toString()))
+            if (tiles.get(i).getType().equals(Type.WATER.toString()))
                 continue;
             for (int j : mesh.getPolygons(i).getNeighborIdxsList()) {
-                if (tiles.get(j).getType().equals(Type.WATER.toString()) || tiles.get(j).getType().equals(Type.LAKE.toString()))
+                if (tiles.get(j).getType().equals(Type.WATER.toString()))
                     valid = false;
             }
             if (valid)
