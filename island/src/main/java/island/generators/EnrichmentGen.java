2dbbcff2159191a7e0c7dc52fac6988d3c90ed36
Ali Virk <ali.m.virk@outlook.com>
2023-03-23 19:43:32 -0400

    Add humidity to tiles neighboring lake tiles for F22

--- island/src/main/java/island/generators/EnrichmentGen.java
+++ island/src/main/java/island/generators/EnrichmentGen.java
@@ -5,11 +5,22 @@ import java.util.List;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import island.Tile.River;
 import island.Tile.Tile;
+import island.Tile.Type;
 
 public class EnrichmentGen {
     
     public List<Tile> enrichLand(Mesh oMesh, List<Tile> tiles, River[] rivers) {
 
+        // Add humidity to land neighboring water
+        for (int i = 0; i < tiles.size(); i++) {
+            if (tiles.get(i).getType().equals(Type.WATER.toString())) {
+                double humidity = tiles.get(i).getHumidity();
+                for (int j : oMesh.getPolygons(i).getNeighborIdxsList()) {
+                    if (tiles.get(j).getType().equals(Type.LAND.toString()))
+                        tiles.get(j).setHumidity(humidity / 4);
+                }
+            }
+        }
 
         return tiles;
     }
