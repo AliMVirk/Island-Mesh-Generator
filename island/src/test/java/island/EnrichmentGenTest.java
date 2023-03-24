849d9015e47fa7831bd95dbe2d2128474f29e3aa
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-23 22:30:58 -0400

    Add test for checking land humidity near rivers for F27

--- island/src/test/java/island/EnrichmentGenTest.java
+++ island/src/test/java/island/EnrichmentGenTest.java
@@ -2,10 +2,14 @@ package island;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import island.Tile.River;
 import island.Tile.Tile;
 import island.Tile.Type;
 import island.Tiles.LandTile;
 import island.generators.EnrichmentGen;
+import island.generators.RiverGen;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +45,28 @@ public class EnrichmentGenTest {
         // Test vegetation
         assertEquals(6.25, ((LandTile)tiles.get(0)).getVegetation());
         assertEquals(0, ((LandTile)tiles.get(2)).getVegetation());
+    }
+
+    @Test
+    public void riverHumiditySpreads() {
+        // Create test mesh
+        Vertex v1 = Vertex.newBuilder().setX(100).setY(100).build();
+        Vertex v2 = Vertex.newBuilder().setX(250).setY(250).build();
+        Segment s = Segment.newBuilder().setV1Idx(0).setV2Idx(1).build();
+        Polygon p = Polygon.newBuilder().addSegmentIdxs(0).setSegmentIdxs(0, 0).build();
+        Mesh aMesh = Mesh.newBuilder().addVertices(v1).addVertices(v2).addSegments(s).addPolygons(p).build();
+
+        // Create corresponding tiles
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(new LandTile());
+
+        // Create rivers
+        RiverGen rgen = new RiverGen();
+        River[] rivers = rgen.createRivers(aMesh, tiles, 1);
+
+        tiles = egen.enrichLand(aMesh, tiles, rivers);
+        // Test humidity
+        assertEquals(rivers[0].getDischarge() * 5, tiles.get(0).getHumidity());
     }
 
 }
