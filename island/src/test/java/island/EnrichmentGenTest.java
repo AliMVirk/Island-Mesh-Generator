55d075063927986d1af1b6a20295de4d08df5c11
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-23 23:55:56 -0400

    Include aquifer test for F33

--- island/src/test/java/island/EnrichmentGenTest.java
+++ island/src/test/java/island/EnrichmentGenTest.java
@@ -73,4 +73,31 @@ public class EnrichmentGenTest {
         assertEquals(rivers[0].getDischarge() * 1.25, ((LandTile)tiles.get(0)).getVegetation());
     }
 
+    @Test
+    public void aquiferHumiditySpreads() {
+        // Create test mesh
+        Polygon p1 = Polygon.newBuilder().addNeighborIdxs(0).setNeighborIdxs(0, 1).build();
+        Polygon p2 = Polygon.newBuilder().addNeighborIdxs(0).setNeighborIdxs(0, 0).build();
+        Polygon p3 = Polygon.newBuilder().build();
+        Mesh aMesh = Mesh.newBuilder().addPolygons(p1).addPolygons(p2).addPolygons(p3).build();
+
+        // Create corresponding tiles
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(new LandTile());
+        tiles.add(new LandTile(100)); // aquifer tile
+        tiles.add(new LandTile());
+
+        tiles = egen.enrichLand(aMesh, tiles, null);
+        // Test humidity
+        assertEquals(25, tiles.get(0).getHumidity());
+        assertEquals(0, tiles.get(2).getHumidity());
+        // Test moisture
+        assertEquals(12.5, ((LandTile)tiles.get(0)).getMoisture());
+        assertEquals(0, ((LandTile)tiles.get(2)).getMoisture());
+        // Test vegetation
+        assertEquals(6.25, ((LandTile)tiles.get(0)).getVegetation());
+        assertEquals(0, ((LandTile)tiles.get(2)).getVegetation());
+    }
+
+
 }
