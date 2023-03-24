400731b80d693f5940cd57dacd6f5f5d4c22a5e1
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-23 23:32:48 -0400

    Include testing for F32

---
+++ island/src/test/java/island/AquiferGenTest.java
@@ -0,0 +1,82 @@
+package island;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
+import island.Tile.Tile;
+import island.Tile.Type;
+import island.Tiles.LandTile;
+import island.generators.AquiferGen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AquiferGenTest {
+
+    private AquiferGen qgen = new AquiferGen();
+
+    @Test
+    public void aquiferExists() {
+        // Create test mesh
+        Vertex v1 = Vertex.newBuilder().setX(0).setY(0).build();
+        Vertex v2 = Vertex.newBuilder().setX(250).setY(250).build();
+        Polygon p1 = Polygon.newBuilder().setCentroidIdx(0).addNeighborIdxs(0).setNeighborIdxs(0, 1).build();
+        Polygon p2 = Polygon.newBuilder().setCentroidIdx(1).addNeighborIdxs(0).setNeighborIdxs(0, 0).build();
+        Mesh aMesh = Mesh.newBuilder().addVertices(v1).addVertices(v2).addPolygons(p1).addPolygons(p2).build();
+
+        // Create corresponding tiles
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(new Tile(Type.LAND, null));
+        tiles.add(new LandTile());
+
+        tiles = qgen.transform(aMesh, tiles, 1);
+        boolean exists = tiles.get(0).getType().equals(Type.AQUIFER.toString()) || tiles.get(1).getType().equals(Type.AQUIFER.toString());
+        assertTrue(exists);
+    }
+
+    @Test
+    public void noAquiferByOcean() {
+        // Create test mesh
+        Vertex v1 = Vertex.newBuilder().setX(0).setY(0).build();
+        Vertex v2 = Vertex.newBuilder().setX(250).setY(250).build();
+        Polygon p1 = Polygon.newBuilder().setCentroidIdx(0).addNeighborIdxs(0).setNeighborIdxs(0, 1).build();
+        Polygon p2 = Polygon.newBuilder().setCentroidIdx(1).addNeighborIdxs(0).setNeighborIdxs(0, 0).build();
+        Mesh aMesh = Mesh.newBuilder().addVertices(v1).addVertices(v2).addPolygons(p1).addPolygons(p2).build();
+
+        // Create corresponding tiles
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(new Tile(Type.WATER, null));
+        tiles.add(new LandTile());
+
+        tiles = qgen.transform(aMesh, tiles, 1);
+        assertEquals(Type.LAND.toString(), tiles.get(1).getType());
+    }
+
+    @Test
+    public void correctHumidity() {
+        // Create test mesh
+        Vertex v1 = Vertex.newBuilder().setX(0).setY(0).build();
+        Vertex v2 = Vertex.newBuilder().setX(250).setY(250).build();
+        Polygon p1 = Polygon.newBuilder().setCentroidIdx(0).build();
+        Polygon p2 = Polygon.newBuilder().setCentroidIdx(1).build();
+        Mesh aMesh = Mesh.newBuilder().addVertices(v1).addVertices(v2).addPolygons(p1).addPolygons(p2).build();
+
+        // Create corresponding tiles
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(new LandTile());
+        tiles.add(new LandTile());
+
+        tiles = qgen.transform(aMesh, tiles, new Random().nextInt(tiles.size()));
+        for (Tile t : tiles) {
+            if (t.getType().equals(Type.AQUIFER.toString()))
+                assertEquals(100, t.getHumidity());
+            else
+                assertEquals(0, t.getHumidity());
+        }
+    }
+
+}
