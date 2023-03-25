3035f84c277b7c12edbdf1f6ba0077f64b1da4f0
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-25 15:52:43 -0400

    Introduce variable soil composition and humidity and moisture spread

--- island/src/main/java/island/generators/EnrichmentGen.java
+++ island/src/main/java/island/generators/EnrichmentGen.java
@@ -1,5 +1,6 @@
 package island.generators;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
@@ -10,18 +11,23 @@ import island.Tiles.LandTile;
 
 public class EnrichmentGen {
 
-    public List<Tile> enrichLand(Mesh oMesh, List<Tile> tiles, River[] rivers) {
+    public List<Tile> enrichLand(Mesh oMesh, List<Tile> tiles, River[] rivers, double[] composition) {
+        List<Integer> assigned = new ArrayList<>();
+        double humidityFactor = composition[0];
+        double moistureFactor = composition[1];
+        int spreadLevel = (int) composition[2];
 
         // Iterate through land neighboring water and set humidity, moisture, and vegetation levels
         for (int i = 0; i < tiles.size(); i++) {
-            if (tiles.get(i).getType().equals(Type.WATER.toString()) || tiles.get(i).getType().equals(Type.AQUIFER.toString())) {
+            if (tiles.get(i).getType().equals(Type.OCEAN.toString()) || tiles.get(i).getType().equals(Type.AQUIFER.toString()) || tiles.get(i).getType().equals(Type.LAKE.toString())) {
                 double humidity = tiles.get(i).getHumidity();
                 for (int j : oMesh.getPolygons(i).getNeighborIdxsList()) {
                     Tile nTile = tiles.get(j);
                     if (nTile.getType().equals(Type.LAND.toString())) {
-                        nTile.setHumidity(humidity / 4);
-                        ((LandTile) nTile).setMoisture(nTile.getHumidity() / 2);
-                        ((LandTile) nTile).setVegetation(((LandTile) nTile).getMoisture() / 2);
+                        nTile.setHumidity(humidity * humidityFactor);
+                        if (!tiles.get(i).getType().equals(Type.OCEAN.toString()))
+                            ((LandTile) nTile).setMoisture(humidity * moistureFactor);
+                        assigned.add(j);
                     }
                 }
             }
@@ -30,14 +36,35 @@ public class EnrichmentGen {
         // Find polygons that have river segments and adjust humidity, moisture, and vegetation accordingly
         for (int i = 0; i < oMesh.getPolygonsCount(); i++) {
             for (int segIdx : oMesh.getPolygons(i).getSegmentIdxsList()) {
-                if (rivers[segIdx] != null) {
-                    Tile nTile = tiles.get(i);
-                    nTile.setHumidity(nTile.getHumidity() + rivers[segIdx].getDischarge() * 5);
-                    ((LandTile) nTile).setMoisture(nTile.getHumidity() / 2);
-                    ((LandTile) nTile).setVegetation(((LandTile) nTile).getMoisture() / 2);
+                Tile t = tiles.get(i);
+                if (rivers[segIdx] != null && t.getType().equals(Type.LAND.toString())) {
+                    t.setHumidity(t.getHumidity() + rivers[segIdx].getHumidity() * humidityFactor);
+                    ((LandTile) t).setMoisture(((LandTile) t).getMoisture() + rivers[segIdx].getHumidity() * moistureFactor);
+                    assigned.add(i);
                 }
             }
         }
+
+        // Spread humidity and moisture to tiles beyond water neighbors
+        for (int n = 0; n < spreadLevel; n++) {
+            List<Integer> toAssign = new ArrayList<>();
+            for (int i = 0; i < assigned.size(); i++) {
+                double humidity = tiles.get(assigned.get(i)).getHumidity();
+                for (int j : oMesh.getPolygons(assigned.get(i)).getNeighborIdxsList()) {
+                    Tile nTile = tiles.get(j);
+                    if (nTile.getType().equals(Type.LAND.toString()) && nTile.getHumidity() < humidity * humidityFactor) {
+                        if (nTile.getHumidity() < humidity * humidityFactor)
+                            nTile.setHumidity(humidity * humidityFactor);
+                        if (((LandTile) nTile).getMoisture() < humidity * moistureFactor)
+                            ((LandTile) nTile).setMoisture(humidity * moistureFactor);
+                        toAssign.add(j);
+                    }
+                }
+            }
+            assigned.clear();
+            assigned.addAll(toAssign);
+        }
+        
 
         return tiles;
     }
