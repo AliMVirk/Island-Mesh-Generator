3834c95bddce8cb2a9a0d2b72688d7eb883a8cd2
Ali Virk <ali.m.virk@outlook.com>
2023-03-26 21:16:22 -0400

    Pass in Random instance for F41

--- island/src/main/java/island/profiles/altitude/Hills.java
+++ island/src/main/java/island/profiles/altitude/Hills.java
@@ -5,20 +5,17 @@ import java.util.Random;
 
 import org.locationtech.jts.geom.Coordinate;
 
-public class Hills implements AltitudeProfile {
+public class Hills {
 
-    @Override
-    public List<Coordinate> buildCoordinates(double width, double height, int numOfMountains) {
+    public List<Coordinate> buildCoordinates(double width, double height, int numOfMountains, Random rnd) {
         List<Coordinate> coords = new ArrayList<>();
-        Random rnd = new Random();
         for (int i = 0; i < numOfMountains; i++) {
             coords.add(new Coordinate(rnd.nextDouble() * width, rnd.nextDouble() * height));
         }
         return coords;
     }
 
-    @Override
-    public AltitudeData build(double width, double height, int numOfMountains, double maxAltitude, double steepnessFactor) {
-        return new AltitudeData(buildCoordinates(width, height, numOfMountains), maxAltitude, steepnessFactor);
+    public AltitudeData build(double width, double height, int numOfMountains, double maxAltitude, double steepnessFactor, Random rnd) {
+        return new AltitudeData(buildCoordinates(width, height, numOfMountains, rnd), maxAltitude, steepnessFactor);
     }
 }
