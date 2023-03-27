0a7263cd6be6d2b497818ac40f11d90d7d4958db
Qamrosh Ahmad <122284734+qamroshahmad@users.noreply.github.com>
2023-03-26 22:57:56 -0400

    Implement seeds for F41 and F42

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
 import org.apache.commons.cli.ParseException;
 import island.configuration.MeshConfiguration;
+import java.util.Random;
 
 public class Main {
     
     public static void main(String[] args) throws ParseException, IOException {
         
-        MeshConfiguration meshConfig = new MeshConfiguration(args);
-        meshConfig.generateIsland();
+        MeshConfiguration meshConfig = new MeshConfiguration(args, new Random().nextInt(9999999));
+        String seed = meshConfig.generateIsland();
+        System.out.println("Seed used " + seed);
 
     }
 }
