791ac8c65dbfd7f2acb6a73c2495cb93c92232d7
Ali Virk <ali.m.virk@outlook.com>
2023-03-29 21:44:54 -0400

    Add key attributes to graph nodes for F44, resolve #1

--- pathfinder/src/main/java/pathfinder/graph/Node.java
+++ pathfinder/src/main/java/pathfinder/graph/Node.java
@@ -2,4 +2,12 @@ package pathfinder.graph;
 
 public class Node {
     
+    final String name;
+    final double altitude;
+
+    public Node(String cityName, double cityAltitude) {
+        name = cityName;
+        altitude = cityAltitude;
+    }
+
 }
