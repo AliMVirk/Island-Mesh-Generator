7bf8fafdd51cb630307102ecdc803000a6d3f91c
Ali Virk <ali.m.virk@outlook.com>
2023-03-29 21:51:56 -0400

    Add key graph edge attributes for F45, resolve #2

--- pathfinder/src/main/java/pathfinder/graph/Edge.java
+++ pathfinder/src/main/java/pathfinder/graph/Edge.java
@@ -2,4 +2,14 @@ package pathfinder.graph;
 
 public class Edge {
     
+    final double weight;
+    final Node N1;
+    final Node N2;
+
+    public Edge(Node node_1, Node node_2, double edgeWeight) {
+        N1 = node_1;
+        N2 = node_2;
+        weight = edgeWeight;
+    }
+
 }
