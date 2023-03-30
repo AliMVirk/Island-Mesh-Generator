b22070043d474e21338e8c06d51156af13e7c155
Ali Virk <ali.m.virk@outlook.com>
2023-03-29 22:01:11 -0400

    Form graph ADT with basic functions for F46, resolve #3

--- pathfinder/src/main/java/pathfinder/graph/Graph.java
+++ pathfinder/src/main/java/pathfinder/graph/Graph.java
@@ -1,15 +1,24 @@
 package pathfinder.graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
     
-    List<Node> N;
-    List<Edge> E;
+    private List<Node> N;
+    private List<Edge> E;
 
     public Graph(List<Node> nodes, List<Edge> edges) {
         N = nodes;
         E = edges;
+    }
+
+    public List<Node> getNodes() {
+        return new ArrayList<>(N);
+    }
+
+    public List<Edge> getEdges() {
+        return new ArrayList<>(E);
     }
 
 }
