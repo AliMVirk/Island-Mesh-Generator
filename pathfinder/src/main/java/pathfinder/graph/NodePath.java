3bd8f5bd274021ae1ef64bec3c2ba1ec6257813e
Ali Virk <ali.m.virk@outlook.com>
2023-04-07 23:10:00 -0400

    Make path and cost immutable

--- pathfinder/src/main/java/pathfinder/graph/NodePath.java
+++ pathfinder/src/main/java/pathfinder/graph/NodePath.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 // Container class for accessing both path and cost maps from Dijsktra's shortest path algorithm
 public class NodePath {
     
-    private HashMap<Node, Node> path;
-    private HashMap<Node, Double> cost;
+    private final HashMap<Node, Node> path;
+    private final HashMap<Node, Double> cost;
 
     public NodePath(HashMap<Node, Node> path, HashMap<Node, Double> cost) {
         this.path = path;
@@ -14,11 +14,11 @@ public class NodePath {
     }
 
     public HashMap<Node, Node> getPath() {
-        return new HashMap<>(path);
+        return path;
     }
 
     public HashMap<Node, Double> getCost() {
-        return new HashMap<>(cost);
+        return cost;
     }
 
 }
