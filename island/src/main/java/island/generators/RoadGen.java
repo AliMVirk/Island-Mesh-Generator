dd9228e687182a12ba525933a783a095d1fe2ae3
Ali Virk <ali.m.virk@outlook.com>
2023-04-12 18:15:09 -0400

    Introduce secondary roads to connect non-capital cities for F51, resolve #12

--- island/src/main/java/island/generators/RoadGen.java
+++ island/src/main/java/island/generators/RoadGen.java
@@ -7,6 +7,7 @@ import java.util.List;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import pathfinder.graph.*;
 
 public class RoadGen {
@@ -20,6 +21,7 @@ public class RoadGen {
         double minMaxPath = Double.MAX_VALUE;
         for (Node n : g.getNodes()) {
             if (Boolean.parseBoolean(n.get("isCity"))) {
+                n.add("isCapital", "false");
                 cities.add(n); // to avoid looping over all nodes again
                 HashMap<Node, Double> cost = g.path.dijkstraShortestPath(n).getCost();
                 double maxPathCost = 0;
@@ -37,26 +39,55 @@ public class RoadGen {
         if (capital == null || minMaxPath == 0) return segments; // no possible city connections exist
         
         // Create paths from the capital to every other city
+        capital.add("isCapital", "true");
+        capital.add("size", "20");
         cities.remove(capital);
-        Property segColor = Property.newBuilder().setKey("rgb_color").setValue("125,128,124").build();
-        Property segThickness = Property.newBuilder().setKey("thickness").setValue("4").build();
         for (Node n : cities) {
             List<Node> path = g.path.findPath(capital, n);
             if (path == null) continue; // no path exists
-            for (int i = 0; i < path.size() - 1; i++) {
-                Segment s = Segment.newBuilder().setV1Idx(Integer.parseInt(path.get(i).get("vertexIndex"))).setV2Idx(Integer.parseInt(path.get(i+1).get("vertexIndex"))).addProperties(segColor).addProperties(segThickness).build();
-                if (!segmentExists(s, segments))
-                    segments.add(s);
+            segments.addAll(createRoad(segments, path, "125,128,124", "7"));
+        }
+
+        // Create paths from random cities to a neighbouring city
+        for (Node n : cities) {
+            if (!n.equals(capital)) {
+                // Find closest city to n
+                Vertex v1 = oMesh.getVertices(Integer.parseInt(n.get("vertexIndex")));
+                Node end = null;
+                double minDistance = Double.MAX_VALUE;
+                for (Node m : cities) {
+                    Vertex v2 = oMesh.getVertices(Integer.parseInt(m.get("vertexIndex")));
+                    if (!n.equals(m) && !m.equals(capital) && Math.sqrt(Math.pow(v1.getX() - v2.getX(), 2) + Math.pow(v1.getY() - v2.getY(), 2)) < minDistance) {
+                        end = m;
+                    }
+                }
+                // Create road
+                List<Node> path = g.path.findPath(n, end);
+                if (path == null) continue; // no path exists
+                segments.addAll(createRoad(segments, path, "162,168,160", "2"));
             }
         }
 
         return segments;
     }
 
+    private List<Segment> createRoad(List<Segment> existingRoads, List<Node> path, String color, String thickness) {
+        List<Segment> segments = new ArrayList<>();
+        Property segColor = Property.newBuilder().setKey("rgb_color").setValue(color).build();
+        Property segThickness = Property.newBuilder().setKey("thickness").setValue(thickness).build();
+
+        for (int i = 0; i < path.size() - 1; i++) {
+            Segment s = Segment.newBuilder().setV1Idx(Integer.parseInt(path.get(i).get("vertexIndex"))).setV2Idx(Integer.parseInt(path.get(i+1).get("vertexIndex"))).addProperties(segColor).addProperties(segThickness).build();
+            if (!segmentExists(s, existingRoads))
+                segments.add(s);
+        }
+        return segments;
+    }
+
     // For preventing addition of duplicate segments
     private boolean segmentExists(Segment seg, List<Segment> segments) {
         for (Segment s : segments) {
-            if (s.getV1Idx() == seg.getV1Idx() && s.getV2Idx() == seg.getV2Idx()) return true;
+            if (s.getV1Idx() == seg.getV1Idx() && s.getV2Idx() == seg.getV2Idx() || s.getV1Idx() == seg.getV2Idx() && s.getV2Idx() == seg.getV1Idx()) return true;
         }
         return false;
     }
--- island/src/test/java/island/RoadGenTest.java
+++ island/src/test/java/island/RoadGenTest.java
@@ -75,30 +75,4 @@ public class RoadGenTest {
         assertTrue(segments.get(0).getV1Idx() == 0 && segments.get(0).getV2Idx() == 1 || segments.get(0).getV1Idx() == 1 && segments.get(0).getV2Idx() == 0);
     }
 
-    @Test
-    public void noRoadsBetweenNonCapitals() {
-        // Create test mesh
-        Vertex v1 = Vertex.newBuilder().setX(0).setY(0).build();
-        Vertex v2 = Vertex.newBuilder().setX(250).setY(250).build();
-        Vertex v3 = Vertex.newBuilder().setX(300).setY(300).build();
-        Polygon p1 = Polygon.newBuilder().setCentroidIdx(0).addNeighborIdxs(0).addNeighborIdxs(1).setNeighborIdxs(0, 1).setNeighborIdxs(1, 2).build();
-        Polygon p2 = Polygon.newBuilder().setCentroidIdx(1).addNeighborIdxs(0).addNeighborIdxs(1).setNeighborIdxs(0, 0).setNeighborIdxs(1, 2).build();
-        Polygon p3 = Polygon.newBuilder().setCentroidIdx(2).addNeighborIdxs(0).addNeighborIdxs(1).setNeighborIdxs(0, 0).setNeighborIdxs(1, 1).build();
-        Mesh aMesh = Mesh.newBuilder().addVertices(v1).addVertices(v2).addVertices(v3).addPolygons(p1).addPolygons(p2).addPolygons(p3).build();
-
-        // Create corresponding tiles
-        List<Tile> tiles = new ArrayList<>();
-        tiles.add(new LandTile());
-        tiles.add(new LandTile());
-        tiles.add(new LandTile());
-
-        // Create corresponding cities
-        Graph g = cgen.generate(aMesh, tiles, 3, new Random());
-
-        List<Segment> segments = rgen.generate(aMesh, g);
-        for (Segment s : segments) {
-            assertTrue(!(s.getV1Idx() == 0 && s.getV2Idx() == 2 || s.getV1Idx() == 2 && s.getV2Idx() == 0));
-        }
-    }
-
 }
